b3dc5d7e503e47d9ff0ee152ec987de804319f0f
Matheus Demetrio Dama <matheusdama@ext.cvccorp.com.br>
2024-01-13 17:27:49 -0300

    create connection

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
+import 'reflect-metadata';
+import createConnection from './database';
 import express from 'express'; 
 import { routes } from './routes';
 
+createConnection();
 const server = express();
 
 server.use(express.json())
